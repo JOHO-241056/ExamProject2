a0500371a21e00f769f429cf7e3bc5329832e691
FujiwaraTakeshi <241056@std.hi-joho.ac.jp>
2025-07-23 10:46:19 +0900

    2025/07/23, 1限

--- src/creature/character/Thief.java
+++ src/creature/character/Thief.java
@@ -2,15 +2,31 @@ package creature.character;
 
 import creature.Character;
 import creature.Creature;
+import weapon.Weapon;
 
 public class Thief extends Character {
-    public Thief(String name, int hp) {
-        super(name, hp);
+    private boolean guard = false;
+
+    public Thief(String name, int hp, Weapon weapon) {
+        super(name, hp, weapon);
     }
 
     public void attack(Creature target) {
-        int damage = 5;
-        System.out.println(getName() + "は素早く攻撃した！" + target.getName() + "に" + damage + "のダメージを与えた！");
+        int damage = getWeapon().getDamage() * 2;
+        System.out.println(getName() + "は"+ getWeapon().getName() + "で素早く2回攻撃した！" + target.getName() + "に" + damage + "のダメージを与えた！");
         target.setHp(target.getHp() - damage);
+    }
+
+    public void guard() {
+        this.guard = true;
+    }
+
+    public void setHp(int hp) {
+        if(this.guard) {
+            this.guard = false;
+            System.out.println("しかし、" + getName() + "は攻撃を回避し、ダメージが入らなかった！");
+        } else {
+            super.setHp(hp);
+        }
     }
 }
